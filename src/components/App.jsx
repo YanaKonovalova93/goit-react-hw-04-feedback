ebbea40042754e8af5671eefd720329b9947a648
YanaKonovalova93 <mikobacteria@gmail.com>
2023-07-09 18:56:50 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import { useState } from 'react';
 import { Statistics } from './Statistic/Statistic';
 import { FeedbackOptions } from './Feedback/FeedbackOptions';
 import { Notification } from './Notification';
